2b05f688ebc0dc3c6533ec515426d0fb13dd714e
anmolvar36 <anmolvar36@gmail.com>
2026-07-17 16:28:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-
 // Basic health check endpoint
 app.get('/api/v1/health', (req, res) => {
   res.status(200).json({ success: true, message: 'Server is running' });
